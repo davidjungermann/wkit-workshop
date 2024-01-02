7f422992416185a1fd6bcfb14093fa776b5c2950
CodeSandbox <support@codesandbox.io>
2024-01-02 12:57:41 +0000

    Sandbox snapshot commit

---
+++ test.py
@@ -0,0 +1,15 @@
+import unittest
+from sum import sum
+
+
+class TestSum(unittest.TestCase):
+
+    def test_sum(self):
+        num1 = 3
+        num2 = 3
+
+        self.assertEqual(sum(num1, num2), 6)
+
+
+if __name__ == '__main__':
+    unittest.main()
